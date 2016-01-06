c35636870f7da5b45eb4b4d6d46ce347d7a4bc0c
Elisha <prophet.elisha@gmail.com>
2016-01-06 12:00:39 -0500

    Working breadth first search

--- kevinbacon.py
+++ kevinbacon.py
@@ -21,7 +21,7 @@ def process_film(film_object):
     for cast in cast_list:
         #actor_name = actor["name"].lower().replace(" ", "")
         cast_name = cast["name"]
-        if cast_name not in cast_list:
+        if cast_name not in actors:
             actors[cast_name] = Actor(cast_name)
             
         actors[cast_name].update_adjacent_actors(cast_list, film_name)
@@ -33,9 +33,32 @@ def read_json():
         with open(os.path.join(path_to_json, js)) as json_file:
             process_film(json.load(json_file))
             
+
+def find_path(input_name):
+    queue = ["Kevin Bacon"]
+    visited = {}
+    count = 0
+    while queue:
+        count += 1
+        current_actor = queue.pop(0)
+        visited[current_actor] = 1
+        if input_name == current_actor:
+            print "Found!"
+            return
+        
+        for actor in actors[current_actor].adjacent_actors.keys():
+            if actor not in visited:
+                queue.append(actor)
+        
+    print count
+            
 if __name__ == "__main__":
     read_json()
     
     while(True):
-        input_name = raw_input("Please enter the actor's name: ")
+        input_name = raw_input("Please enter an actor's name: ")
+        if input_name not in actors:
+            print "Sorry! That actor is not in our data"
+            continue
         
+        find_path(input_name)
