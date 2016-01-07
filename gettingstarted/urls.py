f19d210e971af20da9cf1fbe690ffc27edc3fd77
Elisha <prophet.elisha@gmail.com>
2016-01-07 14:42:16 -0500

    adding django files

---
+++ gettingstarted/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import kevinbacon.views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'gettingstarted.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', kevinbacon.views.index, name='index'),
+    url(r'^db', kevinbacon.views.db, name='db'),
+    url(r'^admin/', include(admin.site.urls)),
+
+)
